251d3bb36c1d1e751764a3caff1aa1ebfcecb65f
Austin <tianyuxin@gmail.com>
2019-10-16 23:25:40 -0400

    revised version

---
+++ SharedPreference/readme.md
@@ -0,0 +1,13 @@
+This is an APP to demo the Shared reference.
+
+Please follow the todo list to understand the code.
+
+The code intends to save the following information on a shared preference list file on cellphone local file storage system.
+
+1. The name and the user.
+
+2. The user settings.
+
+Note:
+
+Needs to update the Settingscreen for an update since the API 28 and later won't support PreferenceFragment and getFragmentManager anymore
--- SharedPreference/src/main/java/com/ceng319/sharedpreferencecode/SettingScreen.java
+++ SharedPreference/src/main/java/com/ceng319/sharedpreferencecode/SettingScreen.java
@@ -6,14 +6,12 @@ Refer to: https://developer.android.com/guide/topics/ui/settings
 */
 
 
-import android.content.res.Resources;
-import android.preference.ListPreference;
-import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
 import android.os.Bundle;
-import android.util.Log;
 
 public class SettingScreen extends AppCompatActivity {
 
@@ -21,20 +19,19 @@ public class SettingScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_screen);
-
         // TODO 7.1: Generate and commit a fragment for the setting screen. finsihed.
-        getFragmentManager().beginTransaction()
+        // getFragmentManager deprecated from API 28 and needs to be updated later on.
+       getFragmentManager().beginTransaction()
                 .add(R.id.idSetting, new SettingFragment())
                 .commit();  // have to commit to submit the transaction.
     }
-
-
 
 
     // Define a Inner Class for setting fragment. This will be used to load into
     // the activity_setting_screen layout.
     // Please notice that Fragment innerclass must be static
     // TODO 7.2: Generate a PreferenceFragment class to hold all the settings build in setting.xml.finsihed.
+    // PreferenceFragment deprecated from API 28 and needs to be updated later on.
     public static class SettingFragment extends PreferenceFragment{
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,5 +39,6 @@ public class SettingScreen extends AppCompatActivity {
             addPreferencesFromResource(R.xml.setting);
         }
     }
+
 
 }
